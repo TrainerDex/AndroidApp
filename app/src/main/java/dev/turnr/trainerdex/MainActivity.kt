824a8b8a09648b74eafecc6e024516b8e3cebcd1
Jay Turner <jaynicholasturner@gmail.com>
2021-08-09 17:17:22 +0100

    Add Splash Screen

--- app/src/main/java/dev/turnr/trainerdex/MainActivity.kt
+++ app/src/main/java/dev/turnr/trainerdex/MainActivity.kt
@@ -1,19 +1,28 @@
 package dev.turnr.trainerdex
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.webkit.CookieManager
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val webView: WebView = findViewById(R.id.webView)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+        val splashScreen: ImageView = findViewById(R.id.splashScreen)
+        splashScreen.bringToFront()
+
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
@@ -22,6 +31,17 @@ class MainActivity : AppCompatActivity() {
         CookieManager.getInstance().setAcceptCookie(true)
 
         webView.loadUrl("https://trainerdex.app/")
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                progressBar.progress = newProgress
+                if (newProgress > 80) {
+                    splashScreen.visibility = View.GONE
+                    webView.visibility = View.VISIBLE
+                }
+                Log.d("Progress", newProgress.toString())
+            }
+        }
 
     }
 
