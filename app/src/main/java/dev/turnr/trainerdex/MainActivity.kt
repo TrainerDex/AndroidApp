09f7533e0dd8e5e04b7dfc63463fb8b3d8083a0f
Jay Turner <jaynicholasturner@gmail.com>
2021-08-09 16:39:52 +0100

    Add WebView

--- app/src/main/java/dev/turnr/trainerdex/MainActivity.kt
+++ app/src/main/java/dev/turnr/trainerdex/MainActivity.kt
@@ -1,11 +1,36 @@
 package dev.turnr.trainerdex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
+        CookieManager.getInstance().setAcceptCookie(true)
+
+        webView.loadUrl("https://trainerdex.app/")
+
+    }
+
+    override fun onBackPressed() {
+        val webView: WebView = findViewById(R.id.webView)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
